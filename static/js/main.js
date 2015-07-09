dfa433f30c61f2f1650d4f317947cda45803bc1a
Jim Grant <jimgrant@gmail.com>
2015-07-08 20:40:10 -0700

    hide edit form on start, reveal when edit button clicked, hide again on cancel

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,15 @@
 $(function() {
     $(".hide-on-start").hide();
+
+    $("#edit-button").on("click", function (event) {
+        event.preventDefault();
+        $(".journal-entry").hide();
+        $("#edit-form-container").show();
+    });
+
+    $(".cancel-button").on("click", function (event) {
+        event.preventDefault();
+        $(".journal-entry").show();
+        $("#edit-form-container").hide();
+    });
 });
