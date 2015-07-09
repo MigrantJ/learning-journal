4a638e2079ee353f1ed0db3379fa634b9f8c7a94
Jim Grant <jimgrant@gmail.com>
2015-07-08 21:07:31 -0700

    ajax call to edit posts. button-back and button-forward changed from css ids to classes

--- static/js/main.js
+++ static/js/main.js
@@ -7,6 +7,30 @@ $(function() {
         $("#edit-form-container").show();
     });
 
+    $("#save-button").on("click", function (event) {
+        event.preventDefault();
+
+        var id = $("#entry-id").val();
+        var title = $("#title").val();
+        var text = $("#text").val();
+
+        $.ajax({
+            method: "POST",
+            url: "/edit/" + id,
+            data: {
+                id: id,
+                title: title,
+                text: text
+            }
+        })
+            .done(function() {
+                alert(id);
+            })
+            .fail(function() {
+                alert( "error" );
+            });
+    });
+
     $(".cancel-button").on("click", function (event) {
         event.preventDefault();
         $(".journal-entry").show();
